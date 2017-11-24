143911fab76197bf3146e335bb3dc510c245bef7
Ryan Finnie <ryan@finnie.org>
2017-11-23 17:46:51 -0800

    Parse imported textfiles

--- m_n_e/collectors/textfile.py
+++ m_n_e/collectors/textfile.py
@@ -1,10 +1,48 @@
-from . import BaseCollector
+from . import BaseCollector, entry
 import os
+import re
+import shlex
 
 
 class Collector(BaseCollector):
+    def postinit(self):
+        if not self.config.textfile_directory:
+            raise NotImplementedError
+        self.re_series = re.compile('^([a-zA-Z_:][a-zA-Z0-9_:]+)(\{.*?\})? ([0-9\-\+e\.]+)$', re.M)
+        self.re_comments = re.compile('^\# ([A-Z][A-Z0-9]+) ([a-zA-Z_:][a-zA-Z0-9_:]+) (.*?)?$', re.M)
+
+    def parse_text(self, text, runseries):
+        for (mname, mlabels, mval) in self.re_series.findall(text):
+            if mname not in runseries:
+                runseries[mname] = {
+                    'labels': [],
+                    'type': 'gauge',
+                    'help': None,
+                }
+            lexer = shlex.shlex(mlabels[1:-1], posix=True)
+            lexer.whitespace_split = True
+            lexer.whitespace = ','
+            runseries[mname]['labels'].append(
+                (
+                    dict(pair.split('=', 1) for pair in lexer),
+                    float(mval),
+                )
+            )
+        for (mtype, mname, mrest) in self.re_comments.findall(text):
+            if mname not in runseries:
+                runseries[mname] = {
+                    'labels': [],
+                    'type': 'gauge',
+                    'help': None,
+                }
+            if mtype == 'HELP':
+                runseries[mname]['help'] = mrest
+            elif mtype == 'TYPE':
+                runseries[mname]['type'] = mrest
+
     def run(self):
-        out = ''
+        runseries = {}
+        out = {}
         path = self.config.textfile_directory
         if not path:
             return
@@ -15,10 +53,18 @@ class Collector(BaseCollector):
         for fn in sorted(promfiles):
             try:
                 with open(os.path.join(path, fn)) as f:
-                    out += f.read()
+                    self.parse_text(f.read(), runseries)
             except (IOError, OSError):
                 continue
-        self.output_raw = out
+        for mname in runseries:
+            if not runseries[mname]['labels']:
+                continue
+            out[mname] = entry(
+                runseries[mname]['labels'],
+                runseries[mname]['type'],
+                runseries[mname]['help'],
+            )
+        self.output = out
 
     def parser_config(self, parser):
         parser.add_argument(
