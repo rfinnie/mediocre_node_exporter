5e217ce3ec8e0b4dc7d9d68c0804e4b3eff6b03a
Ryan Finnie <ryan@finnie.org>
2017-11-24 11:25:00 -0800

    uname: Add domainname if possible

--- m_n_e/collectors/uname.py
+++ m_n_e/collectors/uname.py
@@ -1,18 +1,30 @@
 from . import BaseCollector, entry
 import platform
+import socket
 
 
 class Collector(BaseCollector):
     def run(self):
         out = {}
         uname = platform.uname()
+        domainname = ''
+        try:
+            domainname = '.'.join(
+                socket.getaddrinfo(
+                    uname[1], 0, flags=socket.AI_CANONNAME
+                )[0][3].split('.')[1:]
+            )
+        except:
+            pass
+        if not domainname:
+            domainname = '(none)'
         labels = {
             'sysname': uname[0],
             'nodename': uname[1],
             'release': uname[2],
             'version': uname[3],
             'machine': uname[4],
-            'domainname': '(none)',
+            'domainname': domainname,
         }
         out['node_uname_info'] = entry(
             [(labels, 1)],
